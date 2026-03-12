05df3400715f2f8f3966d35d520baded8bdf3fed
30secondsGK 30secGK <30secondsgk@gmail.com>
2026-03-12 08:45:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-101622214-d421f",
+  "appId": "1:148863280474:web:d70b99a98f2bd3ca0ebf63",
+  "apiKey": "AIzaSyDggCCi5Yzq2b-qNRiK0C-YxD02uWpxAgw",
+  "authDomain": "studio-101622214-d421f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "148863280474"
+};
